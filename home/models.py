94373a87b4ba9234da4b94a4566e10ab8af4c36d
SamariumCode <cactuspydon@gmail.com>
2024-06-10 21:45:56 +0330

    Add file html _base.html

--- home/models.py
+++ home/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField()
 
     def __str__(self):
         return self.title
--- home/views.py
+++ home/views.py
@@ -3,10 +3,9 @@ from .models import Todo
 
 
 def say_hello(request):
-    all = Todo.objects.all()
-    return render(request, 'hello.html', context={'all': all})
+    return render(request, 'hello.html')
 
 
 def index(request):
-    full_name = {'name': 'Admin', 'family': 'Django', 'first': [1, 2, 3, 4], 'second': [5, 6, 7, 8]}
-    return render(request, 'home.html', context={'full_name': full_name})
+    all = Todo.objects.all()
+    return render(request, 'home.html',  context={'todos': all})
