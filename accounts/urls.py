e91df1673ad82d092db4adf85f495eab08b56976
SamariumCode <cactuspydon@gmail.com>
2024-06-11 15:16:58 +0330

    Add Login user

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
 
     path('register/', views.user_register, name='user-register'),
+    path('login/', views.user_login, name='user-login'),
 
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
-from . forms import UserRegistrationForm
+from . forms import UserRegistrationForm, UserLoginForm
 
 
 def user_register(request):
@@ -25,3 +26,24 @@ def user_register(request):
         form = UserRegistrationForm()
 
     return render(request, 'register.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(
+                    request, 'کاربر گرامی شما با موفقیت وارد شدید', extra_tags='success')
+                return redirect('index')
+            else:
+                messages.error(
+                    request, 'نام کاربری یا رمز عبور شما اشتباه هست', extra_tags='danger')
+    else:
+        form = UserLoginForm()
+
+    return render(request, 'login.html', {'form': form})
